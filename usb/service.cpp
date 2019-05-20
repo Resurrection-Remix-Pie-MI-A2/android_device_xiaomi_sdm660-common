0e4992784c9bf23cab8063928260d40ee8b75353
Karanvir Singh <karanvir.singh61@yahoo.in>
2019-05-20 15:12:31 +0530

    Revert "sdm660-common: usb: update usb hals from android-9.0-r39"
    This reverts commit 99039d0a6a3b75484f64ceac541794a9db4f2403.

--- usb/service.cpp
+++ usb/service.cpp
@@ -22,39 +22,35 @@
 # definition file).
 #
  */
- 
- #define LOG_TAG "android.hardware.usb@1.1-service.xiaomi_sdm660"
+
+#include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
 #include "Usb.h"
-#include "UsbGadget.h"
+
 using android::sp;
+
 // libhwbinder:
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
+
 // Generated HIDL files
-using android::hardware::usb::V1_1::IUsb;
-using android::hardware::usb::gadget::V1_0::IUsbGadget;
-using android::hardware::usb::V1_1::implementation::Usb;
-using android::hardware::usb::gadget::V1_0::implementation::UsbGadget;
-using android::OK;
-using android::status_t;
+using android::hardware::usb::V1_0::IUsb;
+using android::hardware::usb::V1_0::implementation::Usb;
+
 int main() {
     android::sp<IUsb> service = new Usb();
-    android::sp<IUsbGadget> service2 = new UsbGadget();
-    configureRpcThreadpool(2, true /*callerWillJoin*/);
-    status_t status = service->registerAsService();
-    if (status != OK) {
-        ALOGE("Cannot register USB HAL service");
+
+    configureRpcThreadpool(1, true /*callerWillJoin*/);
+    android::status_t status = service->registerAsService();
+
+    if (status != android::OK) {
+        LOG(ERROR) << "Cannot register USB HAL service";
         return 1;
     }
-    status = service2->registerAsService();
-    if (status != OK) {
-        ALOGE("Cannot register USB Gadget HAL service");
-        return 1;
-    }
-    ALOGI("USB HAL Ready.");
+
+    LOG(INFO) << "USB HAL Ready.";
     joinRpcThreadpool();
     // Under noraml cases, execution will not reach this line.
-    ALOGI("USB HAL failed to join thread pool.");
+    LOG(ERROR) << "USB HAL failed to join thread pool.";
     return 1;
 }
